42376bedf4c969225ebf64289e216bd666be9131
sathviksimhap <157040751+sathviksimhap@users.noreply.github.com>
2025-02-24 19:47:11 +0530

    Add files via upload

---
+++ Samnsung/DAY 7/Jackpot.py
@@ -0,0 +1,8 @@
+import random
+
+user_number = int(input('Enter a number of your choice between [0 and 9]: '))
+system_number = random.randint(0, 9)
+if system_number != user_number:
+	print('CrorePati')
+else:
+	print('RoadPati')
